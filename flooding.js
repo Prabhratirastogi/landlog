476bd31b6b32e16181b2a1fcdab4aaa9257ea31c
Satyam Tiwary <33483920+satyamTiwary@users.noreply.github.com>
2021-09-29 18:44:23 +0530

    Update flooding.js

--- flooding.js
+++ flooding.js
@@ -1,4 +1,12 @@
 const get_flooding_probability = (long, lat) => {
+  /*
+  ** 
+  */
+  var point_of_interest = ee.Geometry.Point(long, lat);
+  var region = point_of_interest.buffer(4000); // 4000 represents 4km
+  
+  // ... rest of the code
+  
   return "HIGH RISK";
 }
 
