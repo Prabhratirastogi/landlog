9e1f86913069b87a335cafd8f3d43db57fc83469
warriervishnum <87463524+warriervishnum@users.noreply.github.com>
2021-10-19 00:29:57 +0530

    Update flood_occurrance.js

--- compute/flood_occurrance.js
+++ compute/flood_occurrance.js
@@ -1,19 +1,26 @@
-//var longitude=84.74329922964942;
-//var latitude=25.244007840795; 'Sample Location'
+// Require client library and private key.
+var ee = require('@google/earthengine');
+var privateKey = require('./pkey.json');
+var longitude=84.74329922964942;
+var latitude=25.244007840795;
 
-function flood_occurance(longitude,latitude)
-{
-  var location=ee.Geometry.Point(longitude,latitude);                             // Creating Google Earth Engine Point Geometry of Location
-  
-  var flood_data=ee.ImageCollection("GLOBAL_FLOOD_DB/MODIS_EVENTS/V1")
-                .select('flooded').sum();                                        // Derived Flood Occurrance of each pixel using Global Flood Database V1 containing 
-                                                                                  //flood events from 2000 to 2018.
-  
-  var floods_occurred=flood_data.reduceRegion({
+// Initialize client library and run analysis.
+var runAnalysis = function() {
+  ee.initialize(null, null, function() {
+	var location=ee.Geometry.Point(longitude,latitude);                             
+  	var flood_data=ee.ImageCollection("GLOBAL_FLOOD_DB/MODIS_EVENTS/V1").select('flooded').sum();                                         
+	var floods_occurred=flood_data.reduceRegion({
                                                 reducer: ee.Reducer.mean(),
                                                 geometry: location,
                                                 scale:30,
-                                                maxPixels: 1e9});                 //  Retrive the flood occurrance value from the location pixel
-  return floods_occurred.get('flooded');
-  
-}
+                                                maxPixels: 1e9}).getInfo(); 
+	console.log(floods_occurred); 
+  }, function(e) {
+    console.error('Initialization error: ' + e);
+  });
+};
+
+// Authenticate using a service account.
+ee.data.authenticateViaPrivateKey(privateKey, runAnalysis, function(e) {
+  console.error('Authentication error: ' + e);
+});
