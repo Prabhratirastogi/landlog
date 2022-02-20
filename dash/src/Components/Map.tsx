810230e9c7c1e13e9a1a9ec4f4b929b78b47a8e9
Satyam tiwary <satyamtiwary@vvbiotech.com>
2022-02-20 15:45:32 +0530

    fix: fullscreen map

--- dash/src/Components/Map.tsx
+++ dash/src/Components/Map.tsx
@@ -1,8 +1,14 @@
 import React, { useRef, useEffect, useState } from 'react';
 import * as mapboxgl from 'mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
 
+const MapboxAccessToken = 'pk.eyJ1Ijoic2F0eWFtdGl3YXJ5IiwiYSI6ImNrenU5MnNxbTF3cHcyeHBlMTNjZHdlZ2QifQ.LUE5I5Md3DL5yAs0_OUL5A';
+const tcrMapStyles = {
+  minimal: 'mapbox://styles/satyamtiwary/ckzuas3ja000914rx8z59er8a',
+  utility: 'mapbox://styles/satyamtiwary/ckzuyq6st000215ko60wcm7kj'
+};
+
 // Set Access Token for Mapbox
-(mapboxgl as any).accessToken ='pk.eyJ1Ijoic2F0eWFtdGl3YXJ5IiwiYSI6ImNrenU5MnNxbTF3cHcyeHBlMTNjZHdlZ2QifQ.LUE5I5Md3DL5yAs0_OUL5A';
+(mapboxgl as any).accessToken = MapboxAccessToken;
 
 const Map: React.FC = () => {
   const mapContainer = useRef<HTMLDivElement>(null);
@@ -15,7 +21,7 @@ const Map: React.FC = () => {
     if (map.current) return; // initialize map only once
     (map.current as any) = new mapboxgl.Map({
       container: mapContainer.current as HTMLDivElement,
-      style: 'mapbox://styles/satyamtiwary/ckzuas3ja000914rx8z59er8a',
+      style: tcrMapStyles.minimal,
       center: [lng, lat],
       zoom: zoom
     });
@@ -23,7 +29,7 @@ const Map: React.FC = () => {
 
   return(
     // TODO: Map: https://docs.mapbox.com/help/tutorials/use-mapbox-gl-js-with-react/
-    <div ref={mapContainer} className="h-10" />
+    <div ref={mapContainer} className="absolute inset-y-0 h-screen w-screen -z-10" />
   )
 }
 
--- docs/dash/dev-log/1-bootstrapping.md
+++ docs/dash/dev-log/1-bootstrapping.md
@@ -2,3 +2,4 @@
 
 - [Tailwind React Typescript](https://www.hoshki.me/blog/2020-09-11-how-to-setup-tailwind-css-with-cra-and-typescript/)
 - [Mapbox JS](https://docs.mapbox.com/mapbox-gl-js/guides/install/)
+- [Emotion with Tailwind](https://ryanlanciaux.com/blog/2020/01/02/tailwind-with-react/)
