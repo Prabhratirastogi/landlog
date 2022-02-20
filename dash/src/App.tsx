33be99c4b797f73b5c9c82c70cbc7bf4d728371d
Satyam tiwary <satyamtiwary@vvbiotech.com>
2022-02-20 17:15:00 +0530

    ci: test heroku deploy

--- dash/src/App.tsx
+++ dash/src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import Map from './Components/Map';
+import Menubar from './Components/NavBar';
 
 const App: React.FC = () => {
   return (
     // TODO: NavBar: https://tailwindui.com/components/application-ui/navigation/navbars#component-ac4ed72c2e03976dc5415ce711fe2f78
     <div>
-      <input className='h-100 bg-slate-400 z-90' placeholder='hhhh'/>
+      <Menubar />
       <Map />
     </div>
   )
