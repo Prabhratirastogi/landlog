d41a47f6a2dae13f9ced20cdce5ad6e4962a4823
Satyam tiwary <satyamtiwary@vvbiotech.com>
2022-02-28 01:06:35 +0530

    build: try to fix css not loading in prod

--- dash/pages/_app.tsx
+++ dash/pages/_app.tsx
@@ -1,12 +1,10 @@
 import '../styles/globals.css';
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
 import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
 
 function MyApp({ Component, pageProps }: AppProps) {  
     return (
-      <>
-      <Component {...pageProps} />
-      </>
+       <Component {...pageProps} />
     );
 }
 
