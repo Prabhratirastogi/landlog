36e37ef8cb672cbdc0e366358f8fa5498aedc56a
Satyam tiwary <satyamtiwary@vvbiotech.com>
2022-03-10 01:37:33 +0530

    refactor: extract menu to ui

--- apps/landlog-dash/pages/index.tsx
+++ apps/landlog-dash/pages/index.tsx
@@ -7,7 +7,7 @@ import 'mapbox-gl/dist/mapbox-gl.css';
 
 import { useStore } from '../store';
 import { AppLayout } from '../component/AppLayout';
-import LandsMenu from '../component/menu';
+import LandsMenu from 'ui/menu';
 import { LocationMarkerIcon } from '@heroicons/react/solid';
 
 const geojson: GeoJSON.FeatureCollection = {
