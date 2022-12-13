9ea9f45abfd3a509d42c89fcc9a9950756ca1809
bhanu-priyaa <bhanurathore099@gmail.com>
2022-12-13 20:46:25 +0530

    added files

---
+++ firebase.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth} from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyB7GLmN6Y6MuNTRiJ0Q2dx7NvVMBbAywEI",
+  authDomain: "landlog-342017.firebaseapp.com",
+  projectId: "landlog-342017",
+  storageBucket: "landlog-342017.appspot.com",
+  messagingSenderId: "504861541459",
+  appId: "1:504861541459:web:9f3e73db785eaa390b32a6",
+  measurementId: "G-Z37VS5W135"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+export {auth}
---
+++ pages/login/index.tsx
@@ -0,0 +1,139 @@
+import React,{useState} from "react";
+import {signInWithEmailAndPassword , sendEmailVerification} from 'firebase/auth';
+import {auth} from '../../firebase';
+import {useNavigate} from "react-router-dom";
+
+const Login = () => {
+
+    const Navigate = useNavigate();
+
+    const[email,setEmail] = useState('');
+    const[password,setPassword] = useState('');
+    const handleLogin = () => {
+        signInWithEmailAndPassword(auth,email,password)
+        .then(() => {
+            if(!auth.currentUser.emailVerified){
+                sendEmailVerification(auth.currentUser)
+                .then(() => {
+                    useNavigate('/')
+                })
+                .catch(err => alert(err.message))
+            }
+            else{
+                useNavigate('/login')
+            }
+        })
+        .catch(err => alert(err.message))
+    }
+  return (
+    <>
+      <div className="min-h-full flex flex-col justify-center py-12 sm:px-6 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-md">
+          <img
+            className="mx-auto h-12 w-auto"
+            src="/logo/logo512.png"
+            alt="Workflow"
+          />
+          <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">Sign in to your account</h2>
+        </div>
+
+        <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+          <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
+            <form className="space-y-6" action="#" method="POST">
+              
+              <div>
+                <label htmlFor="email" className="block text-sm font-medium text-gray-700">
+                  Email address
+                </label>
+                <div className="mt-1">
+                  <input
+                    id="email"
+                    name="email"
+                    type="email"
+                    onChange = {
+                        (e) => {setEmail(e.target.value)}
+                        
+                    }
+                    autoComplete="email"
+                    required
+                    className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-sky-500 focus:border-sky-500 sm:text-sm"
+                  />
+                </div>
+              </div>
+
+              <div>
+                <label htmlFor="password" className="block text-sm font-medium text-gray-700">
+                  Password
+                </label>
+                <div className="mt-1">
+                  <input
+                    id="password"
+                    name="password"
+                    onChange = {
+                        (e) => {setPassword(e.target.value)}
+                    }
+                    type="password"
+                    autoComplete="current-password"
+                    required
+                    className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-sky-500 focus:border-sky-500 sm:text-sm"
+                  />
+                </div>
+              </div>
+
+              <div className="flex items-center justify-between">
+                <div className="flex items-center">
+                  <input
+                    id="remember-me"
+                    name="remember-me"
+                    type="checkbox"
+                    className="h-4 w-4 text-sky-600 focus:ring-sky-500 border-gray-300 rounded"
+                  />
+                  <label htmlFor="remember-me" className="ml-2 block text-sm text-gray-900">
+                    Remember me
+                  </label>
+                </div>
+
+                <div className="text-sm">
+                  <a href="#" className="font-medium text-sky-600 hover:text-sky-500">
+                    Forgot your password?
+                  </a>
+                </div>
+              </div>
+
+              <div>
+                <button
+                  onClick={handleLogin}
+                  type="submit"
+                  className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-sky-600 hover:bg-sky-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-sky-500"
+                >
+                  Sign in
+                </button>
+              </div>
+            </form>
+
+            <div className="mt-6">
+              <div className="relative">
+                <div className="absolute inset-0 flex items-center">
+                  <div className="w-full border-t border-gray-300" />
+                </div>
+                <div className="relative flex justify-center text-sm">
+                  {/* <span className="px-2 bg-white text-gray-500">Or continue with</span> */}
+                </div>
+              </div>
+                {/* <div>
+                  <a
+                    href="#"
+                    className="w-full inline-flex justify-center py-2 px-4 border border-gray-300 rounded-md shadow-sm bg-white text-sm font-medium text-gray-500 hover:bg-gray-50"
+                  >
+                    <span >Sign in with Google</span>
+                  </a>
+              </div> */}
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Login;
