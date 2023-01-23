2e17fcbd3b8e4157550803dfd0a78dd6774c704a
chandan tiwari <developerchandan1@gmail.com>
2023-01-24 01:16:55 +0530

    fix:privateKey issue

--- lib/firebaseAdmin.ts
+++ lib/firebaseAdmin.ts
@@ -1,6 +1,5 @@
 import * as firebaseAdmin from "firebase-admin";
-
-const privateKey = process.env.NEXT_PUBLIC_FIREBASE_PRIVATE_KEY ? process.env.NEXT_PUBLIC_FIREBASE_PRIVATE_KEY.replace(/\n/gm, "\n") :undefined;
+const privateKey = process.env.NEXT_PUBLIC_FIREBASE_PRIVATE_KEY ? process.env.NEXT_PUBLIC_FIREBASE_PRIVATE_KEY.replace(/\\n/gm, "\n") : undefined;
 const clientEmail = process.env.NEXT_PUBLIC_FIREBASE_CLIENT_EMAIL;
 const projectId = process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID;
 
